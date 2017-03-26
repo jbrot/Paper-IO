b2ff70e0cb446396a2f900d23a54e8d530ed09eb
Chandler Meyers <meyersch@umich.edu>
2017-03-26 16:40:26 -0400

    Implemented Player.cpp

--- server/Player.cpp
+++ server/Player.cpp
@@ -9,4 +9,39 @@
 
 #include "Player.h"
 
-// TODO Implement Player
+id_t Player::getId() {
+	return id;
+}
+
+std::string Player::getName() {
+	return name;
+}
+
+void Player::setX(pos_t newX) {
+	x = newX;
+}
+
+pos_t Player::getX() {
+	return x;
+}
+
+void Player::setY(pos_t newY) {
+	y = newY;
+}
+
+pos_t Player::getY() {
+	return y;
+}
+
+void Player::setLocation(pos_t newX, pos_t newY) {
+	x = newX;
+	y = newY;
+}
+
+bool Player::isDead() {
+	return !alive;
+}
+
+void Player::setDead(bool dead) {
+	alive = dead;
+}
