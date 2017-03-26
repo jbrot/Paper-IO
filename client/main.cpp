f81f46a0c318ef3705610212a4c0d7d238b0e4f9
Joshua Brot <jbrot@umich.edu>
2017-03-26 15:24:43 -0400

    Created stub client main. Created client/server build files. Updated main build file to build two targets.
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

---
+++ client/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * This is the main entry point for the paper-io client. 
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+
+	return app.exec();
+}
